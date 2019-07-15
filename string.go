bb712e8633370c5a8147b8e43070b3fdaa8b7d13
malijie <malijie@360.cn>
2019-07-15 10:22:17 +0800

    新增http build query

--- string.go
+++ string.go
@@ -1,5 +1,11 @@
 package utility
 
+import (
+	"fmt"
+	"net/url"
+	"reflect"
+)
+
 func Substr(s string, start int, length ...int) string {
 	fetchLen := len(s)
 	if len(length) > 0 {
@@ -25,4 +31,46 @@ func Substr(s string, start int, length ...int) string {
 		start, end = end, start
 	}
 	return string(runeSlice[start:end])
+}
+
+//HttpBuildQuery 生成 URL-encode 之后的请求字符串
+func HttpBuildQuery(params map[string]interface{}) string {
+	var urlValue = url.Values{}
+	for k, v := range params {
+		switch v.(type) {
+		case float32:
+			urlValue.Add(k, fmt.Sprintf("%f", v.(float32)))
+		case []float32:
+			for _, sv := range v.([]float32) {
+				urlValue.Add(k+`[]`, fmt.Sprintf("%f", sv))
+			}
+		case float64:
+			urlValue.Add(k, fmt.Sprintf("%f", v.(float64)))
+		case []float64:
+			for _, sv := range v.([]float64) {
+				urlValue.Add(k+`[]`, fmt.Sprintf("%f", sv))
+			}
+		case int:
+			urlValue.Add(k, fmt.Sprintf("%d", v.(int)))
+		case []int:
+			for _, sv := range v.([]int) {
+				urlValue.Add(k+`[]`, fmt.Sprintf("%d", sv))
+			}
+		case int64:
+			urlValue.Add(k, fmt.Sprintf("%d", v.(int64)))
+		case []int64:
+			for _, sv := range v.([]int64) {
+				urlValue.Add(k+`[]`, fmt.Sprintf("%d", sv))
+			}
+		case string:
+			urlValue.Add(k, v.(string))
+		case []string:
+			for _, sv := range v.([]string) {
+				urlValue.Add(k+`[]`, sv)
+			}
+		default:
+			panic( k + " invalid type " + reflect.TypeOf(v).Kind().String())
+		}
+	}
+	return urlValue.Encode()
 }
