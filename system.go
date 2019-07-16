204aa54dd50748268d146c83051a9f3296336070
malijie <malijie@360.cn>
2019-07-16 19:15:21 +0800

    增加Exec 系统调用函数

---
+++ system.go
@@ -0,0 +1,21 @@
+package utility
+
+import (
+	"os/exec"
+	"strings"
+)
+
+//Exec 执行一个外部程序
+//参考 https://stackoverflow.com/questions/10781516/how-to-pipe-several-commands-in-go
+// 例：`ps -eaf|grep "nginx: master"|grep -v "grep"|awk '{print $2}'`
+func Exec(command string, bashEnv ...string) ([]string, error) {
+	bash := `/bin/sh`
+	if 0 < len(bashEnv) {
+		bash = bashEnv[0]
+	}
+	out, err := exec.Command(bash, `-c`, command).Output()
+	if err != nil {
+		return []string{}, err
+	}
+	return strings.Split(strings.Trim(string(out),"\n"), "\n"), nil
+}
